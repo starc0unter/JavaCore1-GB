9f41574932f07d807586558b8d6934d9bca19303
starc0unter <79835070975@yandex.ru>
2018-07-13 15:12:43 +0700

    Made homework 4

---
+++ homework4/src/main/java/ru/chentsov/javacore/windows/ChatWindow.java
@@ -0,0 +1,93 @@
+package ru.chentsov.javacore.windows;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/*Создать окно для клиентской части чата: большое текстовое поле для отображения переписки
+в центре окна. Однострочное текстовое поле для ввода сообщений и кнопка для отсылки
+сообщений на нижней панели. Сообщение должно отсылаться либо по нажатию кнопки на
+форме, либо по нажатию кнопки Enter. При «отсылке» сообщение перекидывается из нижнего*/
+
+/**
+ * @author Evgenii Chentsov
+ */
+public class ChatWindow extends JFrame {
+
+    private final static String DEFAULT_MESSAGE_TEXT = "Write a message... ";
+    private JScrollPane viewMessageScroll;
+    private JTextArea viewMessageArea;
+    private JPanel sendMessagePanel;
+    private JButton sendMessageButton;
+    private JScrollPane sendMessageScroll;
+    private JTextField sendMessageField;
+
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem menuItem;
+
+    public ChatWindow() {
+        viewMessageArea = new JTextArea();
+        viewMessageArea.setLineWrap(true);
+        viewMessageArea.setEditable(false);
+        viewMessageScroll = new JScrollPane(viewMessageArea);
+
+        sendMessageField = new JTextField(DEFAULT_MESSAGE_TEXT, 27);
+        sendMessageField.addFocusListener(getMessageSendFocusListener());
+        sendMessageField.addActionListener(e -> sendMessage());
+
+        sendMessageScroll = new JScrollPane(sendMessageField);
+        sendMessageScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        sendMessageButton = new JButton("Send");
+        sendMessageButton.addActionListener(e -> sendMessage());
+
+        sendMessagePanel = new JPanel(new BorderLayout());
+        sendMessagePanel.add(sendMessageScroll, BorderLayout.CENTER);
+        sendMessagePanel.add(sendMessageButton, BorderLayout.EAST);
+
+        menuBar = new JMenuBar();
+        menu = new JMenu("Window");
+        menuBar.add(menu);
+
+        menuItem = new JMenuItem("Clear");
+        menuItem.addActionListener(e -> viewMessageArea.setText(""));
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem("Exit");
+        menuItem.addActionListener(e -> System.exit(0));
+        menu.add(menuItem);
+
+        setTitle ("Chat Window");
+        add(viewMessageScroll, BorderLayout.CENTER);
+        add(menuBar, BorderLayout.NORTH);
+        getContentPane().add(BorderLayout.SOUTH, sendMessagePanel);
+        setDefaultCloseOperation (WindowConstants.EXIT_ON_CLOSE);
+        setBounds (300 , 300 , 400 , 300);
+        setMinimumSize(new Dimension(400, 300));
+    }
+
+    private void sendMessage() {
+        final boolean hasDefaultMessageText = sendMessageField.getText().equals(DEFAULT_MESSAGE_TEXT);
+        final boolean isMessageEmpty = sendMessageField.getText().isEmpty();
+        if (hasDefaultMessageText || isMessageEmpty) return;
+        final String chatText = viewMessageArea.getText() + sendMessageField.getText() + System.lineSeparator();
+        viewMessageArea.setText(chatText);
+        sendMessageField.setText("");
+    }
+
+    private FocusListener getMessageSendFocusListener() {
+        return new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (DEFAULT_MESSAGE_TEXT.equals(sendMessageField.getText()))  sendMessageField.setText("");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (sendMessageField.getText().isEmpty()) sendMessageField.setText(DEFAULT_MESSAGE_TEXT);
+            }
+        };
+    }
+
+}
